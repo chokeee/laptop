d5a0e1731b7b7dcd16c0bc24a9f251e0ac4ed713
Martin Nemec <martin.nemec@enrian.com>
2022-08-02 22:30:32 +0200

    Update install.sh
    Add Homebrew shell configuration

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 if ! command -v brew > /dev/null 2>&1; then
   echo "Installing homebrew"
   yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/martinnemec/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 if ! command -v git > /dev/null 2>&1; then
